b9446e66ed70985896198d2a54dc9ed29707fd59
Jakub <jakubjagielka2137@gmail.com>
2023-10-25 20:29:41 +0200

    Stworzenie zakaresu sprawdzanych liter

--- Projekt_do_repozytorium/Program.cs
+++ Projekt_do_repozytorium/Program.cs
@@ -1,13 +1,17 @@
-
-﻿
 using Projekt_do_repozytorium;
 
 string tekst = "hefi gvewitbhbiwerh bie hrdigfdisebsoavi faousfdbaosdAsafdfsadfadG ADFdsfsdfdgdxgsdsfdsfd#$%#w$twfafs#wtrfwgsd";
 Histogram h = new Histogram(tekst);
 
-foreach(var element in h.Elementy)
-{ 
-    Console.WriteLine($"{element.Key}:{element.Value}");
+List<char> literkidosprawdzenia = new List<char> { 'A', 'H', 'F', 'D', 'X' };
+
+foreach (var element in h.Elementy)
+{
+    if (literkidosprawdzenia.Contains(element.Key))
+    {
+        Console.WriteLine($"{element.Key}:{element.Value}");
+    }
+
 }
 
 Console.ReadLine();
