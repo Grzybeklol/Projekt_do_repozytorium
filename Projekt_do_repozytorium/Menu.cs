3e28fd95dd07e242f9dd8670b6560b3452cab64d
Jakub Kapłon <jakub.kaplon1@edu.uekat.pl>
2023-10-26 21:19:24 +0200

    Dodano proste menu i możliwość odczytu z klawiatury

---
+++ Projekt_do_repozytorium/Menu.cs
@@ -0,0 +1,30 @@
+﻿using Projekt_do_repozytorium;
+using System;
+
+static class Menu
+{
+    public static string StartMenu()
+    {
+        Console.Title = "Histogram";
+        while (true)
+        {
+            Console.WriteLine("Wybierz metode podania tekstu: ");
+            Console.WriteLine("1 - Z klawiatury");
+            Console.WriteLine("2 - Z url");
+            ConsoleKeyInfo WybranaOpcja = Console.ReadKey();
+            switch(WybranaOpcja.Key) 
+            {
+                case ConsoleKey.D1:
+                    Console.Clear();
+                    Console.WriteLine("Podaj tekst: ");
+                    return Console.ReadLine();
+                case ConsoleKey.D2:
+                    Console.Clear();
+                    Console.WriteLine("Podaj url: ");
+                    string Url = Console.ReadLine(); 
+                    
+                    break;
+            }
+        }
+    }
+}
--- Projekt_do_repozytorium/Program.cs
+++ Projekt_do_repozytorium/Program.cs
@@ -1,12 +1,12 @@
 
 ﻿
 using Projekt_do_repozytorium;
+using System;
 
-string tekst = "hefi gvewitbhbiwerh bie hrdigfdisebsoavi faousfdbaosdAsafdfsadfadG ADFdsfsdfdgdxgsdsfdsfd#$%#w$twfafs#wtrfwgsd";
+
+string tekst=Menu.StartMenu();
 Histogram h = new Histogram(tekst);
-
+Console.Clear();
 h.RysujHistogram();
 
 Console.ReadLine();
-
-// test
